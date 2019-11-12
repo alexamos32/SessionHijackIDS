3497ba1abc2ee530a1a1a1ea045f06620d12195e
alexamos32 <alexanderamos@me.com>
2019-11-12 14:53:19 -0400

    created sniffer that logs arp packets

--- Analyzer.py
+++ Analyzer.py
@@ -1,20 +1,38 @@
-#! /usr/bin/catcpython3
+#! /usr/bin/python3
 
 import sys
 from scapy.all import *
 
+arpDict = {}
+
+#Checking packet type (FTP)
+#def check_pkt_type(pkt):
+ #   if pkt.haslayer(TCP) and pkt.haslayer(Raw):
+  #      if pkt[TCP].dport == 21 or pkt[TCP].sport == 21:
+   #         return True
+    #    else:
+     #       return False
+   # else:
+    #    return False
 
 
-#Checking packet type (ARP)
-def check_pkt_type(pkt):
-    if pkt.haslayer(TCP) and pkt.haslayer(Raw):
-        if pkt[TCP].dport == 21 or pkt[TCP].sport == 21:
-            return True
+#pkts = sniff(filter='arp', count=5)
+#print(pkts.summary())
+
+
+def log_arp_packets(pkt):
+    if ARP in pkt and pkt[ARP].op == 2:
+        srcIP = pkt[ARP].psrc
+        if srcIP in arpDict:
+            arpDict[srcIP] += 1
         else:
-            return False
-    else:
-        return False
+            arpDict[srcIP] = 1
+
+        #pkt.show()
+        print(arpDict)
+        return #pkt.sprintf("%ARP.hwsrc% %ARP.psrc%")
+        
 
 
-pkts = sniff(filter='arp', count=5)
-print(pkts.summary())
+sniff(prn=log_arp_packets, filter='arp', store=0)   
+        
